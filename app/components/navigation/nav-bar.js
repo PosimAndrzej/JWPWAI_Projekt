9b87b811525cb5cbedb66383fe330ee2d6217220
PosimAndrzej <posimandrzej@gmail.com>
2025-02-06 16:53:02 +0100

    Prace nad nawigacją

--- app/components/navigation/nav-bar.js
+++ app/components/navigation/nav-bar.js
@@ -1,12 +1,19 @@
 import NavLink from "./nav-link";
+import logoImg from '@/assets/logo.png';
 
 export default function NavBar() {
 
     return (
-            <nav className="">
-                <ul>
+            <nav className="flex items-center p-8">
+                <NavLink href="/">
+                    <img src={logoImg.src} alt="A plate with food on it" />
+                </NavLink>
+                <ul className="flex g-1"> 
                     <li>
-                        <NavLink href="/">Strona główna</NavLink>
+                        <NavLink href="/autorzy">Autorzy</NavLink>
+                    </li>
+                    <li>
+                        <NavLink href="/autorzy">Autorzy</NavLink>
                     </li>
                     <li>
                         <NavLink href="/autorzy">Autorzy</NavLink>
